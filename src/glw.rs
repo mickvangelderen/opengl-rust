42b88e90ad9fd4964b73f5cf475282e383afa582
Mick van Gelderen <mickvangelderen@gmail.com>
2017-11-01 13:58:24 +0100

    :wrench: Move Program and VertexBuffer to glw
    Add trait ID and an associate type on the Shader trait which must
    implement ID.
    Fixes some public/private issues.

--- src/glw.rs
+++ src/glw.rs
@@ -6,26 +6,165 @@ use std::ffi;
 use std::ptr;
 use core::nonzero::NonZero;
 
+type ValidID = NonZero<GLuint>;
+
+pub trait ID {
+    unsafe fn as_uint(&self) -> GLuint;
+}
+
+pub struct VertexBufferID(ValidID);
+
+impl VertexBufferID {
+    pub fn new() -> Option<Self> {
+        NonZero::new(unsafe {
+            let mut id: GLuint = 0;
+            gl::GenBuffers(1, &mut id);
+            id
+        }).map(VertexBufferID)
+    }
+}
+
+impl ID for VertexBufferID {
+    unsafe fn as_uint(&self) -> GLuint {
+        (self.0).get()
+    }
+}
+
+impl Drop for VertexBufferID {
+    fn drop(&mut self) {
+        unsafe {
+            gl::DeleteBuffers(1, &self.as_uint());
+        }
+    }
+}
+
+pub struct VertexBuffer(VertexBufferID);
+
+impl VertexBuffer {
+    pub fn new() -> Result<Self, String> {
+        let id = VertexBufferID::new().ok_or_else(
+            || String::from("Failed to acquire buffer id.")
+        )?;
+        Ok(VertexBuffer(id))
+    }
+
+    pub fn id(&self) -> &VertexBufferID {
+        &self.0
+    }
+
+    pub fn bind(&self) -> () {
+        unsafe {
+            gl::BindBuffer(gl::ARRAY_BUFFER, self.id().as_uint());
+            // FIXME: Check for errors.
+        }
+    }
+}
+
+pub struct ProgramID(ValidID);
+
+impl ProgramID {
+    pub fn new() -> Option<Self> {
+        NonZero::new(unsafe { gl::CreateProgram() }).map(ProgramID)
+    }
+}
+
+impl ID for ProgramID {
+    unsafe fn as_uint(&self) -> GLuint {
+        (self.0).get()
+    }
+}
+
+impl Drop for ProgramID {
+    fn drop(&mut self) {
+        unsafe {
+            gl::DeleteProgram(self.as_uint());
+        }
+    }
+}
+
+pub struct Program(ProgramID);
+
+impl Program {
+    pub fn new() -> Result<Self, String> {
+        ProgramID::new().map(Program).ok_or_else(|| {
+            String::from("Failed to acquire program id")
+        })
+    }
+
+    pub fn id(&self) -> &ProgramID {
+        &self.0
+    }
+
+    pub fn attach<T: Shader>(&self, shader: &T) {
+        unsafe {
+            gl::AttachShader(self.id().as_uint(), shader.id().as_uint());
+        }
+    }
+
+    pub fn link(&self) -> Result<(), String> {
+        unsafe {
+            gl::LinkProgram(self.id().as_uint());
+        }
+
+        let mut status = gl::FALSE as GLint;
+
+        unsafe {
+            gl::GetProgramiv(self.id().as_uint(), gl::LINK_STATUS, &mut status);
+        }
+
+        if status != (gl::TRUE as GLint) {
+            let mut len = 0;
+
+            unsafe {
+                gl::GetProgramiv(self.id().as_uint(), gl::INFO_LOG_LENGTH, &mut len);
+            }
+
+            let mut buf = Vec::with_capacity(len as usize);
+
+            unsafe {
+                buf.set_len((len as usize) - 1);
+            }
+
+            unsafe {
+                gl::GetShaderInfoLog(
+                    self.id().as_uint(),
+                    len,
+                    ptr::null_mut(),
+                    buf.as_mut_ptr() as *mut GLchar
+                );
+            }
+
+            return Err(String::from_utf8(buf).expect("Program info log is not utf8"))
+        }
+
+        Ok(())
+    }
+}
+
 pub trait Shader {
+    type ShaderID: ID;
+
     fn new(source: ffi::CString) -> Result<Self, String>
     where
         Self: Sized;
 
-    unsafe fn id(&self) -> NonZero<GLuint>;
+    fn id(&self) -> &Self::ShaderID;
 }
 
 macro_rules! impl_shader_type {
     ($XShaderID:ident, $XShader:ident, $SHADER_TYPE:path) => (
-        pub struct $XShaderID(NonZero<GLuint>);
+        pub struct $XShaderID(ValidID);
 
         impl $XShaderID {
             pub fn new() -> Option<Self> {
                 NonZero::new(unsafe { gl::CreateShader($SHADER_TYPE) })
                     .map($XShaderID)
             }
+        }
 
-            pub unsafe fn get(&self) -> NonZero<GLuint> {
-                self.0
+        impl ID for $XShaderID {
+            unsafe fn as_uint(&self) -> GLuint {
+                (self.0).get()
             }
         }
 
@@ -38,6 +177,8 @@ macro_rules! impl_shader_type {
         pub struct $XShader($XShaderID);
 
         impl Shader for $XShader {
+            type ShaderID = $XShaderID;
+
             fn new(source: ffi::CString) -> Result<Self, String> {
 
                 let id = $XShaderID::new().ok_or_else(
@@ -45,24 +186,24 @@ macro_rules! impl_shader_type {
                 )?;
 
                 unsafe {
-                    gl::ShaderSource(id.get().get(), 1, &source.as_ptr(), ptr::null());
+                    gl::ShaderSource(id.as_uint(), 1, &source.as_ptr(), ptr::null());
                 }
 
                 unsafe {
-                    gl::CompileShader(id.get().get());
+                    gl::CompileShader(id.as_uint());
                 }
 
                 let mut status = gl::FALSE as GLint;
 
                 unsafe {
-                    gl::GetShaderiv(id.get().get(), gl::COMPILE_STATUS, &mut status);
+                    gl::GetShaderiv(id.as_uint(), gl::COMPILE_STATUS, &mut status);
                 }
 
                 if status != (gl::TRUE as GLint) {
                     let mut len = 0;
 
                     unsafe {
-                        gl::GetShaderiv(id.get().get(), gl::INFO_LOG_LENGTH, &mut len);
+                        gl::GetShaderiv(id.as_uint(), gl::INFO_LOG_LENGTH, &mut len);
                     }
 
                     let mut buf = Vec::with_capacity(len as usize);
@@ -73,7 +214,7 @@ macro_rules! impl_shader_type {
 
                     unsafe {
                         gl::GetShaderInfoLog(
-                            id.get().get(),
+                            id.as_uint(),
                             len,
                             ptr::null_mut(),
                             buf.as_mut_ptr() as *mut GLchar
@@ -86,8 +227,8 @@ macro_rules! impl_shader_type {
                 }
             }
 
-            unsafe fn id(&self) -> NonZero<GLuint> {
-                (self.0).get()
+            fn id(&self) -> &Self::ShaderID {
+                &self.0
             }
         }
     )
--- src/main.rs
+++ src/main.rs
@@ -13,9 +13,8 @@ use std::fs::File;
 use std::io::Read;
 use gl::types::*;
 use glw::Shader;
+use glw::ID;
 use std::time;
-use core::nonzero::NonZero;
-use std::ptr;
 
 fn main() {
     let mut events_loop = glutin::EventsLoop::new();
@@ -42,7 +41,7 @@ fn main() {
 
     let vertices: [GLfloat; 9] = [-0.5, -0.5, 0.0, 0.5, -0.5, 0.0, 0.0, 0.7, 0.0];
 
-    let vb = VertexBuffer::new().unwrap();
+    let vb = glw::VertexBuffer::new().unwrap();
 
     vb.bind();
 
@@ -64,7 +63,7 @@ fn main() {
         let fragment_shader =
             glw::VertexShader::new(fragment_src).expect("Failed to compile fragment shader.");
 
-        let program = Program::new().unwrap();
+        let program = glw::Program::new().unwrap();
 
         program.attach(&vertex_shader);
         program.attach(&fragment_shader);
@@ -136,60 +135,6 @@ fn main() {
     }
 }
 
-pub struct VertexBufferID(ValidID);
-
-impl VertexBufferID {
-    pub fn new() -> Option<Self> {
-        NonZero::new(unsafe {
-            let mut id: GLuint = 0;
-            gl::GenBuffers(1, &mut id);
-            id
-        }).map(VertexBufferID)
-    }
-
-    pub unsafe fn as_uint(&self) -> GLuint {
-        (self.0).get()
-    }
-}
-
-impl Drop for VertexBufferID {
-    fn drop(&mut self) {
-        unsafe {
-            gl::DeleteBuffers(1, &self.as_uint());
-        }
-    }
-}
-
-pub struct VertexBuffer(VertexBufferID);
-
-impl VertexBuffer {
-    fn new() -> Result<Self, String> {
-        let id = VertexBufferID::new().ok_or_else(
-            || String::from("Failed to acquire buffer id.")
-        )?;
-        Ok(VertexBuffer(id))
-    }
-
-    fn id(&self) -> &VertexBufferID {
-        &self.0
-    }
-
-    fn bind(&self) -> () {
-        unsafe {
-            gl::BindBuffer(gl::ARRAY_BUFFER, self.id().as_uint());
-            // FIXME: Check for errors.
-        }
-    }
-}
-
-impl Drop for VertexBuffer {
-    fn drop(&mut self) {
-        unsafe {
-            gl::DeleteBuffers(1, &self.id().as_uint());
-        }
-    }
-}
-
 fn file_to_cstring<P: AsRef<Path>>(path: P) -> std::io::Result<std::ffi::CString> {
     let file = File::open(path)?;
     let mut reader = std::io::BufReader::new(file);
@@ -197,85 +142,4 @@ fn file_to_cstring<P: AsRef<Path>>(path: P) -> std::io::Result<std::ffi::CString
     reader.read_to_end(&mut bytes)?;
     let string = std::ffi::CString::new(bytes)?;
     Ok(string)
-}
-
-type ValidID = NonZero<GLuint>;
-
-pub struct ProgramID(ValidID);
-
-impl ProgramID {
-    pub fn new() -> Option<Self> {
-        NonZero::new(unsafe { gl::CreateProgram() }).map(ProgramID)
-    }
-
-    pub unsafe fn as_uint(&self) -> GLuint {
-        (self.0).get()
-    }
-}
-
-impl Drop for ProgramID {
-    fn drop(&mut self) {
-        unsafe {
-            gl::DeleteProgram(self.as_uint());
-        }
-    }
-}
-
-pub struct Program(ProgramID);
-
-impl Program {
-    pub fn new() -> Result<Self, String> {
-        ProgramID::new().map(Program).ok_or_else(|| {
-            String::from("Failed to acquire program id")
-        })
-    }
-
-    pub fn id(&self) -> &ProgramID {
-        &self.0
-    }
-
-    pub fn attach<T: Shader>(&self, shader: &T) {
-        unsafe {
-            gl::AttachShader(self.id().as_uint(), shader.id().get());
-        }
-    }
-
-    pub fn link(&self) -> Result<(), String> {
-        unsafe {
-            gl::LinkProgram(self.id().as_uint());
-        }
-
-        let mut status = gl::FALSE as GLint;
-
-        unsafe {
-            gl::GetProgramiv(self.id().as_uint(), gl::LINK_STATUS, &mut status);
-        }
-
-        if status != (gl::TRUE as GLint) {
-            let mut len = 0;
-
-            unsafe {
-                gl::GetProgramiv(self.id().as_uint(), gl::INFO_LOG_LENGTH, &mut len);
-            }
-
-            let mut buf = Vec::with_capacity(len as usize);
-
-            unsafe {
-                buf.set_len((len as usize) - 1);
-            }
-
-            unsafe {
-                gl::GetShaderInfoLog(
-                    self.id().as_uint(),
-                    len,
-                    ptr::null_mut(),
-                    buf.as_mut_ptr() as *mut GLchar
-                );
-            }
-
-            return Err(String::from_utf8(buf).expect("Program info log is not utf8"))
-        }
-
-        Ok(())
-    }
 }
